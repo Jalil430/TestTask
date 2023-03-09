35d140bd80a961010491ab804dc9ff456dac54e1
Jalil <jalil.katalov.417@gmail.com>
2023-03-09 23:56:37 +0300

    Sign in UI

---
+++ TeskTask/app/src/main/java/com/example/testtask/view/ui/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testtask.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testtask.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_activity)
+    }
+}
